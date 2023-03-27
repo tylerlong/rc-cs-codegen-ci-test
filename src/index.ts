b3a2c95a8650419787ccc8192898c5d517345268
Tyler Long <tyler4long@gmail.com>
2023-03-27 14:11:55 -0700

    Try new ringcentral-open-api-parser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+import { prepareSpec } from 'ringcentral-open-api-parser';
+
 import { generateDefinitions } from './definitions';
 import { generatePaths } from './paths';
 import { generateSamples } from './samples';
-import { parseSpec } from './utils';
 
 let index = process.argv.indexOf('-i');
 let inputFile = '';
@@ -17,7 +18,7 @@ if (index !== -1) {
   outputDir = process.argv[index + 1];
 }
 
-const parsed = parseSpec(inputFile);
+const parsed = prepareSpec(inputFile);
 generateDefinitions(parsed, outputDir);
 generatePaths(parsed, outputDir);
 generateSamples(parsed, outputDir);
--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,3 @@
-import { parse } from 'ringcentral-open-api-parser';
-import * as yaml from 'js-yaml';
-import * as fs from 'fs';
-import { OpenAPIV3 } from 'openapi-types';
-
 export const capitalizeFirstLetter = (s: string): string => {
   return s.charAt(0).toUpperCase() + s.slice(1);
-};
-
-export const parseSpec = (inputFile: string) => {
-  const doc = yaml.load(fs.readFileSync(inputFile, 'utf8')) as OpenAPIV3.Document;
-  return parse(doc);
 };
